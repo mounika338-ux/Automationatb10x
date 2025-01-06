f9875f63022facb4e928b31f9ad6bf76864ad41e
mounika338-ux <paravasthu338@gmail.com>
2025-01-06 11:21:48 +0530

    initial commit

--- src/jan5thTask/javaexercise3.java
+++ src/jan5thTask/javaexercise3.java
@@ -3,6 +3,8 @@ package jan5thTask;
 public class javaexercise3 {
     public static void main(String[] args)
     {
-        System.out.println( "im gud ");
+        int a=10+20;
+        int b=2+3;
+        System.out.println("sum of a and b :"+(a+b));
     }
 }
