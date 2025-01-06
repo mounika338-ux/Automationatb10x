1b0d5b688343aa9e941478b85f89733b5e22eebc
mounika338-ux <paravasthu338@gmail.com>
2025-01-06 08:02:22 +0530

    java

--- src/jan5thTask/javaexercise2.java
+++ src/jan5thTask/javaexercise2.java
@@ -4,5 +4,7 @@ public class javaexercise2 {
     public static void main ( String[]args )
     {
         System.out.println("how are you");
+        //we can commit no.of lines or
+        //multi lines we can commit
     }
 }
