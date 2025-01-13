791977d7f4354cb4d690fa9363d47cf2c0ea81f1
mounika338-ux <paravasthu338@gmail.com>
2025-01-13 18:53:00 +0530

    second commit

---
+++ src/jan11thTask/tableexercise2.java
@@ -0,0 +1,12 @@
+package jan11thTask;
+
+public class tableexercise2 {
+    public static void main(String[] args) {
+        int num=9;
+        for(int i=1;i<=10;i++)
+        {
+            int num1 = num * i;
+            System.out.printf("%d*1=%d\n",9,num1);
+    }
+
+}}
