3f03551827d6f791df9ca7cc8fa1bc916dacdf76
mounika338-ux <paravasthu338@gmail.com>
2025-01-13 18:43:45 +0530

    second commit

---
+++ src/jan11thTask/tableexercise.java
@@ -0,0 +1,17 @@
+package jan11thTask;
+
+public class tableexercise {
+    public static void main(String[] args) {
+        System.out.println("table of nine");
+        System.out.printf("9 * 1 = %d\n" , 9);
+        System.out.printf("9*2 = %d\n" , 18);
+        System.out.printf("9*3 = %d\n" , 27);
+        System.out.printf("9*4 = %d\n" , 36);
+        System.out.printf("9*5 = %d\n" , 40);
+        System.out.printf("9*6 = %d\n" , 54);
+        System.out.printf("9*7 = %d\n" , 63);
+        System.out.printf("9*8 = %d\n" , 72);
+        System.out.printf("9*9 = %d\n" , 81);
+        System.out.printf("9*10 = %d\n" , 90);
+    }
+}
