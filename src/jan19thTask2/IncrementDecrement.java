198a46c5a79eba17d232f4da9bae196bdd74222a
mounika338-ux <paravasthu338@gmail.com>
2025-01-19 20:44:37 +0530

    fourth commit

---
+++ src/jan19thTask2/IncrementDecrement.java
@@ -0,0 +1,35 @@
+package jan19thTask2;
+
+public class IncrementDecrement {
+    /* Finding the Largest of Three Numbers a,b,c using the Ternary Operator.
+
+    public static void main(String[] args) {
+        int a=1;
+        int b=4;
+        int c=3;
+        int result=a>b ?(a>c ?a:c ):(b>c ? b:c);
+        // int result = a > b ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println(result);*/
+
+    /*public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a + a++ + a++);//(11+12+13)
+        System.out.println(a);
+    }}*/
+
+    /*public static void main(String[] args) {
+        int a = 20;
+        System.out.println("—a + a++ + a—");
+        System.out.println(a);
+
+
+    }}*/
+
+
+
+
+
+
+
+
+
